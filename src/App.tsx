c762321253ce7f74f170e344868aacfa0a55244e
Lalaina Razafindraibe <lraza@equinor.com>
2025-07-09 12:47:13 +0200

    Installed router to use of the other components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import './App.css'
-import ParkingDisplay from './components/ParkingDisplay'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ParkingDisplay from './components/ParkingDisplay';
 
 function App() {
   return (
-    <div>
-      <h1>Parking management</h1>
-      <ParkingDisplay />
-    </div>
-  )
+    <Router>
+      <div>
+        <h1>Parking management</h1>
+        <Routes>
+          <Route path="/" element={<ParkingDisplay />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
