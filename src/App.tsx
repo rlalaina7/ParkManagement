5f14684d61abb4e346b757b7c0ff9ac567493e1d
Lalaina Razafindraibe <lraza@equinor.com>
2025-07-09 17:10:00 +0200

    Set the styling for the display garage with tailwin

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ParkingDisplay from './components/ParkingDisplay';
+import ParkingDisplay from './components/Parking/ParkingDisplay';
 
 function App() {
   return (
     <Router>
-      <div>
-        <h1>Parking management</h1>
         <Routes>
           <Route path="/" element={<ParkingDisplay />} />
         </Routes>
-      </div>
     </Router>
   );
 }
